e4888e423c62782a267f7900241b8e05f3907cc1
Cassyel <cassyel1967@gmail.com>
2024-03-04 22:32:03 -0300

    Add JWT Token on Login

--- src/services/login/index.ts
+++ src/services/login/index.ts
@@ -1,10 +1,15 @@
 import { LoginDto } from '../../controllers/login/dto/login.dto';
 import { LoginResponse } from '../../controllers/login/dto/login.response';
+import { env } from '../../env';
 import User from '../../models/User';
+import jwt from 'jsonwebtoken';
 
 export class LoginService {
   async login(userData: LoginDto) {
     const response = new LoginResponse();
+
+    const user = new User();
+    user.email = userData.email;
 
     try {
       const user = await User.findOne({ where: { email: userData.email } });
@@ -15,10 +20,11 @@ export class LoginService {
         response.success = false;
         response.errorMessage = 'Credenciais inválidas';
       } else {
-        // Credenciais válidas
+        const tokenJwt = jwt.sign({ email: user.email, id: user.id }, env.JWT_SECRET);
+
         response.code = 200;
         response.success = true;
-        response.token = 'ADICIONAR TOKEN JWT';
+        response.token = tokenJwt;
       }
     } catch (error) {
       // Erro interno do servidor
