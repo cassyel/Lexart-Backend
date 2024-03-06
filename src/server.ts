bf32365800c88473d342a9ccb00a658304ccafdc
Cassyel <cassyel1967@gmail.com>
2024-03-05 21:43:51 -0300

    Change server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,28 @@
-import express, { Express } from 'express';
+import express from 'express';
 import sequelize from './config/database';
 import cors from 'cors';
 import ErrorHandler from './middlewares/errorHandling';
 import router from './routes/router';
 
-class App {
-  private expressApp: Express;
+export default async function setupServer() {
+  const app = express();
 
-  constructor() {
-    this.expressApp = express();
-  }
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão efetuada');
 
-  public async setup() {
-    try {
-      await sequelize.authenticate();
-      this.expressApp.listen(3000, () => console.log('servidor rodando na porta 3000'));
-      // await sequelize.sync();
-      console.log('Conexão efetuada');
+    app.use(express.json());
+    app.use(cors());
+    app.use(router);
+    app.use(ErrorHandler.handleServerError);
 
-      this.expressApp.use(express.json());
-      this.expressApp.use(cors());
-      this.expressApp.use(router);
-      this.expressApp.use(ErrorHandler.handleServerError);
-    } catch (error) {
-      console.error('Erro ao conectar com o banco de dados:', error);
-      sequelize.close();
-    }
-  }
-
-  public getExpressApp(): Express {
-    return this.expressApp;
+    const port = 3333;
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+    sequelize.close();
   }
 }
 
-// Exporta a função que cria e inicializa o servidor
-export default async function createServer(): Promise<Express> {
-  const app = new App();
-  await app.setup();
-  return app.getExpressApp();
-}
-
-// Se estiver executando como script principal, inicia o servidor
-// if (require.main === module) {
-//   const PORT = process.env.PORT || 3000;
-//   createServer().then((app) => {
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   });
-// }
+// Para iniciar o servidor, basta chamar a função setupServer
+setupServer();
