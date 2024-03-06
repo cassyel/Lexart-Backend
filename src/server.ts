f2e8453861c556bbfd2a79455b5481fc0fb6bf58
Cassyel <cassyel1967@gmail.com>
2024-03-06 00:05:57 -0300

    Apply all Configs To Deploy Vercel

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,22 @@
 import express from 'express';
-import sequelize from './config/database';
 import cors from 'cors';
 import ErrorHandler from './middlewares/errorHandling';
 import router from './routes/router';
 
-async function createServer() {
-  const app = express();
-  const corsConfig = {
-    origin: '*',
-    credential: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-  };
+const app = express();
+const corsConfig = {
+  origin: '*',
+  credential: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+};
 
-  try {
-    await sequelize.authenticate();
-    console.log('Conexão efetuada');
+console.log('Conexão efetuada');
 
-    app.use(express.json());
-    app.options('', cors(corsConfig));
-    app.use(cors(corsConfig));
-    app.use(router);
-    app.use(ErrorHandler.handleServerError);
+app.use(express.json());
+app.options('', cors(corsConfig));
+app.use(cors(corsConfig));
+app.use(router);
+app.use(ErrorHandler.handleServerError);
 
-    app.listen(3000, () => console.log(`Criando o servidor em: ${new Date()}`));
 
-    return app; // Retornando a instância do servidor
-  } catch (error) {
-    console.error('Erro ao conectar com o banco de dados:', error);
-    await sequelize.close(); // Fechando a conexão com o banco de dados em caso de erro
-    throw error; // Relançando o erro para que o Vercel possa tratar
-  }
-}
-
-createServer();
-
-export default createServer;
+export default app;
